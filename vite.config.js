ccf1e13fec45e2221527aa70b9b0e48a810bcb26
miroslav.blascanin <miroslav.blascanin@espas.ch>
2024-12-17 15:10:55 +0100

    fixed vite manifest

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/bootstrap-4.0.0-dist/css/bootstrap.css'
             ],
             refresh: true,
         }),
